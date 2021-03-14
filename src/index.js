eae073bfa82b3019f420fb50db6f6da277c006e5
hataevicha5922 <hataevicha5922@gmail.com>
2021-03-14 21:49:20 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    let res = 0;
+    if (arguments.length !== 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < res) {
+                res = array[i];
+            }
+        }
+    } else {
+        res = 0;
+    }
+    return res;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let res = 0;
+    if (arguments.length !== 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > res) {
+                res = array[i];
+            }
+        }
+    } else {
+        res = 0;
+    }
+    return res;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    let res = 0;
+    if (arguments.length !== 0) {
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+            res = sum / array.length;
+        }
+    } else {
+        res = 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return res;
+};
